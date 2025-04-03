b8bcb6448b80227ea20f52871b7291ea0a55a576
biba736 <semenkoptev06@gmail.com>
2025-04-03 20:18:28 +0300

    Update config

--- 7_1.py
+++ 7_1.py
@@ -1,16 +1,30 @@
+def binSearch(A, elem):
+    # если искомый элемент за границами диапазона,то делать нечего
+    if elem < A[0] or elem > A[-1]:
+        return None
+
+    # определим верхнюю границу и вызовем рекурсивную функцию
+    return binSearchRec(A, elem, 0, len(A) - 1, [])
+
+
 def binSearchRec(A, elem, left, right, B):
+    # если подмассив пустой, то делать нечего
     if left > right:
-        return -1
-    mid = (left + right)//2
+        return B
+
+    # определяем средний элемент
+    mid = (left + right) // 2
     B.append(A[mid])
+
+    # выполняем сравнение и рекурсивный вызов на одной из половин
     if A[mid] == elem:
-        print(" ".join(map(str, B)))
+        return B
     elif A[mid] > elem:
         return binSearchRec(A, elem, left, mid - 1, B)
     else:
         return binSearchRec(A, elem, mid + 1, right, B)
 
+
 elem = int(input())
 A = list(map(int, input().split()))
-B = []
-binSearchRec(A, elem, 0, len(A) - 1, B)
+print(" ".join(map(str, binSearch(A, elem))))
